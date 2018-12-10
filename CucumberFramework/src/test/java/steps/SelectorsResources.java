03188a81e4a404f5471161ac6eb8520951e77394
Byras <byra.lukasz@gmail.com>
2018-12-11 00:10:49 +0100

    Almost finished, add pomin to instead of board

--- CucumberFramework/src/test/java/steps/SelectorsResources.java
+++ CucumberFramework/src/test/java/steps/SelectorsResources.java
@@ -2,8 +2,20 @@ package steps;
 
 public class SelectorsResources {
 
-	public static final String SIGN_UP_LINK = "body > div.global-header.mod-trello-blue.mod-dark-background.u-clearfix > div.global-header-section.mod-right > a.global-header-section-button.mod-primary";
+	public static final String SIGN_UP_BUTTON = "body > div.global-header.mod-trello-blue.mod-dark-background.u-clearfix > div.global-header-section.mod-right > a.global-header-section-button.mod-primary";
 	public static final String SIGN_UP_IT_IS_FREE = "body > div.layout-centered.mod-white-text.u-center-text.header-section > div:nth-child(1) > a";
 	public static final String SELECT_LANGUAGE = "#language-picker";
-
+	public static final String TRELLO_LOGO = ".global-header-section-logo-image";
+	public static final String SIGN_IN_LINK_ON_CREATE = ".sign-in-account > a:nth-child(1)";
+	public static final String CREATE_NEW_ACCOUNT = "#signup";
+	public static final String NAME_INPUT = "#name";
+	public static final String EMAIL_INPUT = "#email";
+	public static final String INVALID_EMAIL_MESSAGE = "#email-error";
+	public static final String PASSWORD_INPUT = "#password";
+	public static final String INCORRECT_PASSWORD_MESSAGE = "#error";
+	public static final String GOOGLE_BUTTON = "#google";
+	public static final String CONTINUE_ARROW = "div.first-board-continue-footer:nth-child(1)";
+	public static final String BOARD_NAME_INPUT = "#content > form > div.first-board-column.first-board-content-wrapper > div.first-board-content > div.first-board-text-wrapper > div > div.board-name > input";
+	public static final String INITIALS_ICON = "span.member > span:nth-child(1)";
+	public static final String LOGOUT = ".js-logout";
 }
